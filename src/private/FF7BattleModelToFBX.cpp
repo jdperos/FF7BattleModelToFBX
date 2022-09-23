be1f6f3ffab460d6707550d0dcb8d09ca9a06695
jdperos <jdperos@gmail.com>
2022-09-23 06:34:15 -0400

    Removing irrelevant code to avoid confusion

--- src/private/FF7BattleModelToFBX.cpp
+++ src/private/FF7BattleModelToFBX.cpp
@@ -5,8 +5,6 @@
 
 int main()
 {
-    uint8* Buffer;
-    uint32 FileSize = FF7::Reader::BattleModel::ReadBattleModel( Buffer, "D:\\FF7\\CLOUD.BIN" );
     FF7::BattleModel::FModel Model( "D:\\FF7\\CLOUD.BIN" );
     
     return 0;
